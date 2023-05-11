eca5b010ef70a8acad369b2607c708da33a0718e
haardie <128052666+haardie@users.noreply.github.com>
2023-05-11 19:04:33 +0200

    Update scaling_eq.py

--- scaling_eq.py
+++ scaling_eq.py
@@ -3,19 +3,21 @@ from scipy.special import kv
 import numpy as np
 
 # define the 'scaling function' and its derivative
+
+
 def f(alpha, beta, lambda_):
     arg = 2 * np.sqrt(beta * lambda_)
-    K = kv(alpha + 2, arg) / kv(alpha + 1, arg)
-    B = np.sqrt(beta / lambda_)
-    return B*K - 1  # Eq
+    k = kv(alpha + 2, arg) / kv(alpha + 1, arg)
+    b = np.sqrt(beta / lambda_)
+    return b*k - 1  # Eq
 
 
 def df(alpha, beta, lambda_):
     arg = 2 * np.sqrt(beta * lambda_)
-    K = kv(alpha + 2, arg) / kv(alpha +1, arg)
-    B = np.sqrt(beta / lambda_)
-    dEq = -B*K*(alpha+2)/lambda_ + (B*K)**2 - B**2
-    return dEq  # dEq/dlambda
+    k = kv(alpha + 2, arg) / kv(alpha + 1, arg)
+    b = np.sqrt(beta / lambda_)
+    d_eq = -b * k * (alpha + 2) / lambda_ + (b * k) ** 2 - b ** 2
+    return d_eq  # dEq/dlambda
 
 
 # define the Newton-Raphson method
@@ -31,6 +33,7 @@ def nr_method(f, df, lambda_0, alpha, beta, max_iter, tolerance, step_size_facto
 
     return lambda_, lambda_vals, i + 1
 
+
 # set tex interpreter and define colors
 plt.rcParams['text.usetex'] = True
 
@@ -42,35 +45,39 @@ colors = ['#b7094c', '#a01a58', '#892b64', '#723c70', '#5c4d7d', '#455e89', '#2e
           '#00b3c5']
 greens = [lime, '#9fcf30', '#60cd70']
 
-# SINGLE ROOT
-# set parameters and the initial guess for lambda(alpha, beta)
+# set parameters
+max_iters = 100
+tol = 1e-6
+step_size_factor = 0.5
+
 alpha = -1.0
 beta = 2.0
-lambda0 = 1.0
+init = 1.0
+
+domain = np.linspace(0.001, 5, 1000)
 
 # solve the scaling equation using the Newton-Raphson method
-lambda_nr, lambdas_nr, iterations_nr = nr_method(f, df, lambda0, alpha, beta, 500, 1e-6, 0.5)
-print(f'lambda({alpha}, {beta}): {lambda_nr}, init.: {lambda0}, iterations: {iterations_nr}')
+solution, iterative_solutions, iters = nr_method(f, df, init, alpha, beta, max_iters, tol, step_size_factor)
+print(f'lambda({alpha}, {beta}): {solution}, init.: {init}, iterations: {iters}')
 print()
 
 # plot the scaling function and its root
-lambdas = np.linspace(0.001, 10, 1000)
-y = [f(alpha, beta, l) for l in lambdas]
-plt.plot(lambdas, y, linewidth=2.4,
+
+plt.plot(domain, [f(alpha, beta, lambda_) for lambda_ in domain], linewidth=2.4,
          label=r'$f(\lambda(\alpha, \beta))\equiv \sqrt{\frac{\beta}{\lambda}}\frac{\mathcal{K}_{\alpha + 1}(2\sqrt{'
                r'\beta\lambda})}{\mathcal{K}_{\alpha}(2\sqrt{\beta\lambda})} - 1 = 0$',
          color=colors[0], zorder=0)
 
-plt.scatter(lambdas_nr, [0] * len(lambdas_nr), label='Newton-Raphsonovy iterované odhady', color=colors[4], s=40,
+plt.scatter(iterative_solutions, [0] * len(iterative_solutions), label='Newton-Raphsonovy iterované odhady', color=colors[4], s=40,
             zorder=1, alpha=0.4)
 
-plt.scatter(lambda0, 0, label=r'Počáteční volba $\lambda = \lambda_0$', color=colors[6], s=50, zorder=2,
+plt.scatter(init, 0, label=r'Počáteční volba $\lambda = \lambda_0$', color=colors[6], s=50, zorder=2,
             edgecolors=lime, linewidth=1)
 
-plt.scatter(lambda_nr, 0, label=r'Aproximace řešení: $\lambda(\alpha, \beta) \doteq$ {:.3f}'.format(lambda_nr),
+plt.scatter(solution, 0, label=r'Aproximace řešení: $\lambda(\alpha, \beta) \doteq$ {:.3f}'.format(solution),
             color=lime, s=200, marker='*', zorder=3, edgecolors=colors[6], linewidth=1)
 
-plt.axvline(x=lambda_nr, color='#bdbbbb', linestyle='--', linewidth=1.2, zorder=0)
+plt.axvline(solution, color='#bdbbbb', linestyle='--', linewidth=1.2, zorder=0)
 plt.axhline(y=0, color='#bdbbbb', linestyle='--', linewidth=1.2, zorder=0)
 plt.xlim(0, 3)
 plt.ylim(-0.3, 2.0)
@@ -80,31 +87,29 @@ plt.legend()
 plt.savefig('scaling_eq_sol.png', dpi=600)
 plt.show()
 
-# MULTIPLE ROOTS
-alpha_arr = np.linspace(-1, 5.0, 10)
-beta_arr = np.linspace(0.2, 5.0, 10)
-lambda0_arr = np.linspace(0.8, 6.0, 10)
+
+alphas = np.linspace(-1, 5.0, 10)
+betas = np.linspace(0.2, 5.0, 10)
+inits = np.linspace(0.8, 6.0, 10)
 
 lambda_nr_arr = []
 
-for c, i in enumerate(range(len(alpha_arr[:3]))):
-    alpha = alpha_arr[1]
-    beta = beta_arr[i]
-    lambda0 = lambda0_arr[i]
-    lambda_nr, lambdas_nr, iterations_nr = nr_method(f, df, lambda0_arr[i], alpha, beta, 50, 1e-5, 0.5)
+for c, i in enumerate(range(len(alphas[:3]))):
+    alpha = alphas[1]
+    beta = betas[i]
+    init = inits[i]
+    solution, iterative_solutions, iters = nr_method(f, df, inits[i], alpha, beta, max_iters, tol, step_size_factor)
 
-    print(f'lambda_{i + 1}: {lambda_nr}, init.: {lambda0_arr[i]}, iterations: {iterations_nr}')
+    print(f'lambda_{i + 1}: {solution}, init.: {inits[i]}, iterations: {iters}')
 
-    lambda_nr_arr.append(lambda_nr)
+    lambda_nr_arr.append(solution)
 
-    y_mult = [f(alpha, beta, l) for l in lambdas]
-
-    plt.plot(lambdas, y_mult, linewidth=2.4, label=r'$f(\lambda(\alpha_2, \beta_{}))$'.format(i + 1), color=colors[c],
+    plt.plot(domain, [f(alpha, beta, lambda_) for lambda_ in domain], linewidth=2.4, label=r'$f(\lambda(\alpha_2, \beta_{}))$'.format(i + 1), color=colors[c],
              zorder=0)
 
-    plt.scatter(lambda0_arr[i], 0, color=greens[i], s=50, zorder=2)
+    plt.scatter(inits[i], 0, color=greens[i], s=50, zorder=2)
     plt.scatter(lambda_nr_arr[i], 0, color=greens[i], s=200, marker='*', zorder=3, edgecolors=colors[6], linewidth=1.2)
-    plt.scatter(lambdas_nr, [0] * len(lambdas_nr), color=colors[c + 4], s=30, zorder=1, alpha=0.4)
+    plt.scatter(iterative_solutions, [0] * len(iterative_solutions), color=colors[c + 4], s=30, zorder=1, alpha=0.4)
     plt.axhline(y=0, color='#bdbbbb', linestyle='--', linewidth=1.2, zorder=0)
 
 plt.ylim(-0.2, 1.0)
@@ -120,25 +125,25 @@ plt.show()
 lambda_sols = []
 asymptote = []
 
-for i in range(len(beta_arr)):
-    alpha = alpha_arr[1]
-    beta = beta_arr[i]
-    lambda0 = lambda0_arr[i]
-    lambda_nr, lambdas_nr, iterations_nr = nr_method(f, df, lambda0_arr[i], alpha, beta_arr[i], 100, 1e-6, 0.5)
+for i in range(len(betas)):
+    alpha = alphas[1]
+    beta = betas[i]
+    init = inits[i]
+    solution, iterative_solutions, iters = nr_method(f, df, inits[i], alpha, betas[i], max_iters, tol, step_size_factor)
 
-    lambda_sols.append(lambda_nr)
+    lambda_sols.append(solution)
     asymptote.append(beta + 3 / 2)
 
-plt.plot(beta_arr, asymptote, linewidth=2.4, label=r'$\lambda(\beta) = \beta + \textstyle\frac{3}{2}$', linestyle='--',
+plt.plot(betas, asymptote, linewidth=2.4, label=r'$\lambda(\beta) = \beta + \textstyle\frac{3}{2}$', linestyle='--',
          color='#bdbbbb', zorder=0)
 
-plt.scatter(beta_arr, lambda_sols, s=30, label=r'$\lambda = \lambda(\alpha_2, \beta)$', color=colors[0],
+plt.scatter(betas, lambda_sols, s=30, label=r'$\lambda = \lambda(\alpha_2, \beta)$', color=colors[0],
             zorder=0)
 
 plt.xlabel(r'$\beta$')
 plt.ylabel(r'$\lambda(\alpha, \beta)$')
 plt.ylim(0, max(lambda_sols) + 0.5)
-plt.xlim(0.0, max(beta_arr) + 0.5)
+plt.xlim(0.0, max(betas) + 0.5)
 plt.legend()
 plt.savefig('asympt_scaling_eq.png', dpi=600)
 plt.show()
